ed2c347a82d417ca6fbbc7b3064d695db103b43a
Daniel Petrov <37422337+daniel-petrov@users.noreply.github.com>
2021-11-06 18:50:39 +0000

    Delete cdqn.py

--- src/cdqn.py
+++
@@ -1,6 +0,0 @@
-# CONTINUOUS DRL LIBRARY
-# continuous action DQN using Tensorforce: https://github.com/tensorforce/tensorforce
-
-# ENVIRONMENT ADAPTERS
-# ALE - framework for developing AI for Atari games: https://github.com/mgbellemare/Arcade-Learning-Environment
-# PGLE - quick start to reinforcement learning in PyGame: https://github.com/ntasfi/PyGame-Learning-Environment/
